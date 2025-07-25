345da1e02d9ba3a0bacc48cd0e618c8f0213df1c
luckyChauhan12w-kodr <luckychauhan.12w@gmail.com>
2025-07-26 01:21:38 +0530

    Validation Form

--- JavaScript/task11/App.js
+++ JavaScript/task11/App.js
@@ -1,3 +1,50 @@
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-})
+const userName = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+function setError(invalidFields) {
+
+    document.querySelectorAll('input').forEach((e) => {
+        e.style.border = 'none'
+    })
+
+
+    invalidFields.forEach(field => {
+
+        switch (field) {
+            case 'username':
+                console.log(userName);
+                userName.style.border = '2px solid red';
+                break;
+            case 'email':
+                email.style.border = '2px solid red';
+                break;
+            case 'password':
+                password.style.border = '2px solid red';
+                break;
+        }
+    });
+
+}
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const fieldList = {
+        username: /^[a-zA-Z0-9_]{4,}$/.test(userName.value),
+        email: /^\S+@\S+\.\S+$/.test(email.value),
+        password: /^(?=.*\d).{7,}$/.test(password.value)
+    }
+
+    if (Object.values(fieldList).every(e => e == true)) {
+        alert('Your LOGIN Is Completed')
+    } else {
+
+        const invalidFields = Object.entries(fieldList)
+            .filter(([field, isValid]) => !isValid)
+            .map(([field]) => field);
+
+        setError(invalidFields)
+    }
+
+});
